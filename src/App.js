7f8dd687de82037b822865822f93f262f98be701
serhatcigerci <yserhatcigerci@gmail.com>
2023-08-12 17:46:03 +0300

    Pulling data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Search from './Search';
 import axios from 'axios';
+import { useState } from 'react';
+import ImageList from './components/ImageList';
 
 function App() {
 
@@ -17,13 +18,16 @@ function App() {
     return response.data.results
   }
 
-  const handleSubmit = (term) => {
-    console.log(term)
-    searchImages(term)
+  const [images, setImages] = useState([])
+
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term)
+    setImages(result)
   }
   return (
     <div className="App">
       <Search search={handleSubmit} />
+      <ImageList imagesPlaceHolder={images} />
     </div>
   );
 }
---
+++ src/components/ImageItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const ImageItem = ({ image }) => {
+    console.log(image)
+  return (
+    <div></div>
+  )
+}
+
+export default ImageItem
